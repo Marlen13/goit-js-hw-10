8c236bf839e1616ae3e5e941879df261018bb0a9
Marlen13 <114424710+Marlen13@users.noreply.github.com>
2023-05-30 12:45:12 +0000

    Deploying to gh-pages from @ Marlen13/goit-js-hw-10@1d4cb8c1c58fe170a09f1b23cf4e31ec9b4bc0fa 🚀

--- index.03ce8cbb.js
+++
@@ -1,2 +0,0 @@
-fetch("https://api.thecatapi.com/v1/breeds?api_key=live_nRyBlc19ILgE4nLWfQo9Nn1yYwfBI4JaTQSD12pVrfB9zC5p2razV1tS3IpPtcIw").then((e=>{if(!e.ok)throw new Error(e.status);return e.json()})).then((e=>{console.log(e);const t=document.querySelector(".breed-select"),n=e.map({id:id,name:name});t.innerHTML=n})).catch((e=>{console.log(e)}));
-//# sourceMappingURL=index.03ce8cbb.js.map
