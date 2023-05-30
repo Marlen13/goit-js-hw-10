1d4cb8c1c58fe170a09f1b23cf4e31ec9b4bc0fa
Marlen13 <olenamarinova@gmail.com>
2023-05-30 15:44:46 +0300

    add changes

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,3 +1,7 @@
+const BASE_URL = 'https://api.thecatapi.com/v1';
+const API_KEY = 'live_nRyBlc19ILgE4nLWfQo9Nn1yYwfBI4JaTQSD12pVrfB9zC5p2razV1tS3IpPtcIw';
+
+
 export function fetchBreeds() {
  return  fetch(`${BASE_URL}/breeds?api_key=${API_KEY}`)
     .then(response => {
